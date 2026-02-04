f3fa6f73ab0bd3ce446d8a5671158c2a90803007
Manuel <puglisimanuel94@gmail.com>
2026-02-04 13:27:38 +0100

    Logo arrotondato e testo aggiornato

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-// Importazione Componenti
+// Importazione dei Componenti
 import LoginView from './components/LoginView.vue'
 import AdminDashboard from './components/AdminDashboard.vue'
 import ReceptionDashboard from './components/ReceptionDashboard.vue'
@@ -11,13 +11,14 @@ import GovernanteDashboard from './components/GovernanteDashboard.vue'
 import StaffDashboard from './components/StaffDashboard.vue'
 import MaintenanceDashboard from './components/MaintenanceDashboard.vue'
 
+// Configurazione delle Rotte (Il "Navigatore" dell'App)
 const routes = [
   { path: '/', component: LoginView },
   { path: '/admin', component: AdminDashboard },
-  { path: '/reception', component: ReceptionDashboard },     // Reception -> Reception
-  { path: '/governante', component: GovernanteDashboard },   // Governante -> Governante
-  { path: '/staff', component: StaffDashboard },             // Staff -> STAFF (Controlla questa riga!)
-  { path: '/manutenzione', component: MaintenanceDashboard }, // Manutenzione -> Manutenzione
+  { path: '/reception', component: ReceptionDashboard },
+  { path: '/governante', component: GovernanteDashboard },
+  { path: '/staff', component: StaffDashboard },
+  { path: '/manutenzione', component: MaintenanceDashboard },
 ]
 
 const router = createRouter({
@@ -25,10 +26,17 @@ const router = createRouter({
   routes,
 })
 
+// Controllo Accessi (Sicurezza base)
 router.beforeEach((to, from, next) => {
+  const userRole = localStorage.getItem('htlfix_user')
   const hotelId = localStorage.getItem('htlfix_hotel_id')
-  if (to.path !== '/' && !hotelId) next('/')
-  else next()
+
+  // Se non sei loggato in un hotel e non sei sulla pagina di login, torna indietro
+  if (to.path !== '/' && !hotelId) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 const app = createApp(App)
